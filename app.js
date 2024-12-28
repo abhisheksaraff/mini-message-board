c29b02963dff45fb35ddad7337c0dd2a634ebbdb
a6hi <saraffabhishek@outlook.com>
2024-12-28 03:08:27 -0500

    added functionality to post new message

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const indexRouter = require("./routes/indexRouter");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use("/", indexRouter);
 app.use(express.urlencoded({ extended: true })); // required to parse the form data into req.body to read form data
+app.use("/", indexRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
